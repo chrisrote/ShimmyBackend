ce1db2543f93210b76a3639d0ac4712d098fe39f
Tyler Harrington <tyler.harrington@gmail.com>
2014-05-11 16:32:51 -0500

    added create tenant

--- test/routes.js
+++ test/routes.js
@@ -1,32 +1,9 @@
 var should = require('should');
 var assert = require('assert');
-var request = require('supertest');
+var supertest = require('supertest');
 var mongoose = require('mongoose');
 var winston = require('winston');
-
-describe('Routing', function() {
-  var host_url = 'localhost:5000';
-
-  // run all setup operations
-  before(function(done) {
-    mongoose.connect('mongodb://th:Shimmy@ds053638.mongolab.com:53638/shimmy');
-    done();
-  });
-
-  describe('Properties', function() {
-    it('should return properties', function(done) {
-      request(host_url).get('/api/property').send().end(function(err, res) {
-        if(err) {
-          throw err;
-        }
-
-        res.should.have.status(200);
-        done();
-      });
-    });
-  });
-});
-
+var api = supertest('http://localhost:5000');
 
 describe('tenant api: ', function() {
     var host = 'localhost:5000'
@@ -38,7 +15,7 @@ describe('tenant api: ', function() {
 
     describe('create new tenant', function() {
         it('should return a new tenant', function(done) {
-            request(host).post('/api/createNewTenant').send().end(function(err, res) {
+            supertest.request(host).post('/api/createNewTenant').send().end(function(err, res) {
                 if(err) throw err;
                 res.should.have.status(200);
             });
